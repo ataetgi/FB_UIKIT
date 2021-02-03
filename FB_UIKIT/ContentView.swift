60a205ab2f0abab7b713fb2835c75e3e8ddfc35d
ataetgi <ataetgi@hotmail.com>
2021-02-03 23:37:22 +0300

    Initial Commit

---
+++ FB_UIKIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB_UIKIT
+//
+//  Created by Ata Etgi on 3.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

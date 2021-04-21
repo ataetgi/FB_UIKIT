7fa4ae162c6f5c2ea3f7270facdc435075e21ae7
ataetgi <ataetgi@icloud.com>
2021-04-21 16:29:22 +0300

    MainViewControler and base UICollectionView added

---
+++ FB_UIKIT/MainController.swift
@@ -0,0 +1,69 @@
+//
+//  MainController.swift
+//  FB_UIKIT
+//
+//  Created by Ata Etgi on 3.02.2021.
+//
+
+import UIKit
+import LBTATools
+
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(backgroundColor: .red)
+    let nameLabel = UILabel(text: "Name Label")
+    let dateLabel = UILabel(text: "Friday at 11:11AM")
+    let postTextLabel = UILabel(text: "Here is my post text")
+    let imageViewGrid = UIView(backgroundColor: .yellow)
+    
+    override func setupViews() {
+        backgroundColor = .white
+        
+        stack(hstack(imageView.withHeight(40).withWidth(40),
+                     stack(nameLabel, dateLabel),
+                     spacing: 8
+        ).padLeft(12).padRight(12).padTop(12),
+        postTextLabel,
+        imageViewGrid,
+        spacing: 8)
+        
+    }
+}
+
+class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        
+        self.items = ["hello", "world", "1", "2"]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
+    }
+    
+}
+
+
+import SwiftUI
+
+// USING SWIFTUI PREVIEW WITH UIKIT
+// CMD + OPT + P => Resume Preview Shortcut
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+//        Text("Main preview")
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}

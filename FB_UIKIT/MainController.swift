1aa98fb5466447f7080988c48d68393d63551b52
ataetgi <ataetgi@icloud.com>
2021-04-21 18:36:03 +0300

    Header section and gradient layer completed

--- FB_UIKIT/MainController.swift
+++ FB_UIKIT/MainController.swift
@@ -32,14 +32,97 @@ class PostCell: LBTAListCell<String> {
     }
 }
 
-class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemYellow
+        stack(storiesController.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet{
+            imageView.image = UIImage(named: item)
+            nameLabel.text = item
+        }
+    }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    
+    let nameLabel = UILabel(text: "Jane Doe", font: .boldSystemFont(ofSize: 14), textColor: .white)
+    
+    override func setupViews() {
+        
+        imageView.layer.cornerRadius = 10
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        stack(UIView(), nameLabel).withMargins(.allSides(8))
+    }
+    
+    let gradient = CAGradientLayer()
+    
+    fileprivate func setupGradientLayer() {
+        
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradient.locations = [0.7, 1.1]
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        layer.addSublayer(gradient)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradient.frame = bounds
+    }
+}
+
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.showsHorizontalScrollIndicator = false
+        self.items = ["jane1", "lady4c","jane2", "lady5c", "jane3", "jane4", "jane1", "kelly2"]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.height / 2, height: view.frame.height - 24)
+    }
+    
+    
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.showsVerticalScrollIndicator = false
         collectionView.backgroundColor = UIColor(white: 0.9, alpha: 1)
         
-        self.items = ["hello", "world", "1", "2"]
+        self.items = ["hello", "world", "1", "3"]
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
